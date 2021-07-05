d92087ee37f59ccf2ed5518c6c8360e7e50d8243
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-07-05 18:39:34 +0200

    API connected and styled, discover page DONE

--- routes/discover.routes.js
+++ routes/discover.routes.js
@@ -8,11 +8,12 @@ router.get('/discover', (req, res, next) => {
     UserModel.findById(_id)
     .then((user) => {
         axios.get(('https://gh-trending-api.herokuapp.com/repositories'))
-        .then((apiResponse) => {
-            res.render("main/discover", {user, apiResponse }) 
+        .then((response) => {
+            res.render("main/discover", {user, response }) 
         })
     })
     .catch((err) => {
+        console.log("Cannot get ai data")
         next(err)
     })
 })
@@ -22,13 +23,14 @@ router.post('/discover', (req, res, next) => {
     const {_id} = req.session.loggedInUser
     console.log(language)
 
-    axios.get(`https://gh-trending-api.herokuapp.com/repositories/${language}?since=daily`)
+    axios.get(`https://gh-trending-api.herokuapp.com/repositories/${language}?since=daily&spoken_lang=en`)
     .then((response) => {
         UserModel.findById(_id)
         .then((user) => {
+
            res.render("main/discover", {user, response}) 
         })
-        // console.log(response.data)
+
     })
     .catch((err) => {
         console.log("could not get an api")
