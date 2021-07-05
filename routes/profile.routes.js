8cccb50cac5e489956a892f3e2e6032d3d491cb5
Kalande Mwamba <kalandekupe@outlook.com>
2021-07-05 12:02:51 +0200

    Profile page v1

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const UserModel = require('../models/User.model');
 const SnippetModel = require('../models/Snippet.model');
+const bcrypt = require('bcryptjs')
 
 router.get('/home', (req, res, next) => {
     const {search} = req.query
@@ -30,12 +31,21 @@ router.get('/home', (req, res, next) => {
 
 router.post('/home', (req, res, next) => {
     const {title,content} = req.body;
-    if (!title) {
-        res.render('main/home', {error: 'Title is required'})
-        return;
-    }
-
     const {_id,posts} = req.session.loggedInUser
+
+    if (!title) {
+        UserModel.findById(_id)
+        .populate('posts')
+        .then((user) => {
+           let posts = user.posts
+           res.render('main/home', {posts, error: 'Title is required'})
+           
+        })
+        .catch(() => {
+            next(err)
+        })
+        return;    
+    }
 
     SnippetModel.create({title,content,owner: _id,})
         .then((post) => { 
@@ -53,10 +63,21 @@ router.post('/home', (req, res, next) => {
 
 router.get('/home/:id/delete', (req, res, next) => {
     let dynamicId = req.params.id;
-
+    const {_id, posts} = req.session.loggedInUser
     SnippetModel.findByIdAndDelete(dynamicId)
         .then(() => {
-            res.redirect('/home')
+            let index = posts.indexOf(dynamicId)
+            posts.splice(index,1)
+            UserModel.findByIdAndUpdate(_id, {posts: posts}, {new: true})
+            .then((user) => {
+                req.session.loggedInUser = user
+                res.redirect('/home')
+            })
+            .catch(() => {
+                
+            })
+            
+            
         })
         .catch(() => {
             next("Delete failed")
@@ -75,6 +96,54 @@ router.get('/discover', (req, res, next) => {
     .catch((err) => {
         next(err)
     })
+})
+
+router.get('/myprofile', (req, res, next) => {
+    const {_id} = req.session.loggedInUser
+    UserModel.findById(_id)
+    .then((user) => {
+        res.render('main/myprofile', {user})
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+router.post('/myprofile', (req, res, next) => {
+    const {username, email, password} = req.body
+    const {_id} = req.session.loggedInUser
+
+    if(!password){
+        UserModel.findByIdAndUpdate(_id, {username, email}, {new: true})
+        .then(() => {
+            res.redirect('/myprofile')
+        })
+        .catch(() => {
+            res.render('main/myprofile' , {error: 'Username already taken'})
+        })
+        return;
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
+    let passcheck = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,14}$/
+    if(!passcheck.test(password)){
+        res.render('main/myprofile', {error: `Password must have:
+        - At least one Number
+        - At least one Special characters
+        - Must be between 6 and 14 characters`})
+        return;
+    }
+
+    UserModel.findByIdAndUpdate(_id, {username, email, password: hash}, {new: true})
+    .then((user) => {
+        req.session.loggedInUser = user
+        res.redirect('/myprofile')
+    })
+    .catch(() => {
+        res.render('main/myprofile' , {error: 'Username already taken'})
+    }) 
 })
 
 module.exports = router;
